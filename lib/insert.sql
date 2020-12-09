c097f3791d6409054b34f820c1f152f7c9f3d84a
romeroraven <romeroravenr@gmail.com>
2020-12-09 11:28:31 -0700

    commit tree 935d839527df42c413f7e6d94dc2ecd9a1349455

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate', '3', 'M', 'Brown', 'Happy', 'Alive');
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', '6', 'M', 'Brown', 'Silent', 'Alive');
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', '13', 'F', 'Brown', 'Happy', 'Alive');
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', '60', 'F', 'Brown', 'Tired', 'Alive');
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', '40', 'M', 'Brown', 'Happy', 'Alive');
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', '30', 'M', 'Brown', 'Meam', 'Alive');
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', '3', 'F', 'Brown', 'Happy', 'Alive');
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ( NULL, '3', 'M', 'Brown', 'Happy', 'Alive');
